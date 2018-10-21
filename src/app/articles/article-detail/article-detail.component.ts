a8fa6f0d1f7de6c192424ad51e4e6ff1ef2da6bb
Divesh Jethani <divesh.jethani@nagarro.com>
2018-10-21 17:02:52 +0530

    article details added

--- src/app/articles/article-detail/article-detail.component.ts
+++ src/app/articles/article-detail/article-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArticlesService } from 'src/app/services/articles/articles.service';
 
 @Component({
   selector: 'app-article-detail',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleDetailComponent implements OnInit {
 
-  constructor() { }
+  comment : string
+  articleData: any;
+
+  constructor(private route: ActivatedRoute, private articleService: ArticlesService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(
+      params => {
+        let slug = params['params'].slug;
+        this.articleService.getArticleDetails(slug).subscribe((data : any) => {
+        this.articleData = data.article;
+        })
+      }
+    )
+  }
+
+  postComment(comment){
+    this.comment = comment
+    console.log(comment)
   }
 
 }
